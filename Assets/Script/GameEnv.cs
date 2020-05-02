861ee6508cffec47b1a3ce541d981fcefba2fb29
tigercosmos <phy.tiger@gmail.com>
2020-05-03 03:11:09 +0800

    skybox

---
+++ Assets/Script/GameEnv.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnv : MonoBehaviour
+{
+    public SkyboxController m_skyboxController;
+    public List<Environment> m_envs = null;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            m_skyboxController.NewEnvironment(m_envs[1]);
+        }
+    }
+}
+
+[Serializable]
+public class Environment
+{
+    public int m_worldRotation = 0;
+    public Texture m_background = null;
+    public AudioClip m_ambientNoise = null;
+}
