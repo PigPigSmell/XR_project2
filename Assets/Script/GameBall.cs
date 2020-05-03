896575230ddb160ac04e028f9084058a23ae5b15
tigercosmos <phy.tiger@gmail.com>
2020-05-03 19:38:23 +0800

    game panel

--- Assets/Script/GameBall.cs
+++ Assets/Script/GameBall.cs
@@ -15,7 +15,7 @@ public class GameBall : MonoBehaviour
         var coordinate = Random.Range(-7, 7);
         while(coordinate > -1.5 && coordinate < 1.5)
         {
-            coordinate = Random.Range(-5, 5);
+            coordinate = Random.Range(-7, 7);
         }
         return coordinate;
     }
