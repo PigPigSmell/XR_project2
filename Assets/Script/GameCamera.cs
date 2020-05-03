892e3a6aa6d61b84da26ae757ec1b99bdce3caea
tigercosmos <phy.tiger@gmail.com>
2020-05-04 02:08:22 +0800

    update

--- Assets/Script/GameCamera.cs
+++ Assets/Script/GameCamera.cs
@@ -7,8 +7,9 @@ public class GameCamera : MonoBehaviour
     public float speedH = 2.0f;
     public float speedV = 2.0f;
 
-    private float yaw = 0.0f;
-    private float pitch = 0.0f;
+    private float yaw = 40.0f;
+    private float pitch = 50.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +19,11 @@ public class GameCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        yaw += speedH * Input.GetAxis("Mouse X");
-        pitch -= speedV * Input.GetAxis("Mouse Y");
+        // yaw += speedH * Input.GetAxis("Mouse X");
+        // pitch -= speedV * Input.GetAxis("Mouse Y");
+         yaw += speedH * Input.acceleration.x;
+         pitch -= speedV * Input.acceleration.y;
 
-        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+         transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
     }
 }
